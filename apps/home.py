6a660354284ee29624c1cc053f90b7cad9e87767
mmon2lprd <62737847+mmon2lprd@users.noreply.github.com>
2021-10-21 00:53:01 +0700

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -5,24 +5,7 @@ from DB_CONNECT import condb
 import numpy as np
 mydb = condb()
 c = mydb.cursor()
-def select_pil(params, outfile=None):
-    sql_cmd = """
-        SELECT vetImg FROM vettable WHERE vetId = %s limit 1;
-    """
-    c.execute(sql_cmd, params)
-    row = c.fetchone()
-    if row:
-        bytes_stream = BytesIO(row[0])
-        img = Image.open(bytes_stream)
-        return img
-    return None
 
 def app():
     st.title('หน้าแรก')
-    st.image(select_pil([3]))
-    none = Image.open('none.png')
-    st.image(none)
-    st.write("")
-    test = ['', 'A', 'B']
-    result = st.selectbox('จังหวัด :', test)
-    st.write(f'.... {result}')
+    st.write("ยินดีต้อนรับ")
